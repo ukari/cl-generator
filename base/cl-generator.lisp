f07fb5f99ea7124e26b5af4fcabf345417acec34
ukari <muromi.ukari@gmail.com>
2018-05-06 19:17:09 +0900

    fix bug cause by multipart body

--- base/cl-generator.lisp
+++ base/cl-generator.lisp
@@ -7,23 +7,20 @@
     `(call/cc (lambda (,x) (make-iterable-object :next ,x :value ,arg)))))
 
 (defmacro lambda* (args &body body)
-  (let ((dmz (gensym)))
-    `(lambda ,args
+  `(lambda ,args
        (with-call/cc
-	 (lambda (&rest ,dmz)
-	   (declare (ignore ,dmz))
-	   (make-iterable-object :next nil :value ,@body))))))
+	 (lambda ()
+	   (make-iterable-object :next nil :value (progn ,@body))))))
 
-(defmacro lambda-yield (args &body body)
-  (let ((dmz (gensym)))
-    `(with-call/cc
-       (lambda (,@args &rest ,dmz)
-	 (declare (ignore ,dmz))
-	 (make-iterable-object :next nil :value ,@body)))))
+(defmacro lambda-yield (&body body)
+  `(with-call/cc
+       (lambda ()
+	 (make-iterable-object :next nil :value (progn ,@body)))))
 
 (defmacro defun* (name args &body body)
   `(defun ,name ,args (lambda-yield () ,@body)))
 
 (defmacro defmacro* (name args &body body)
   `(defmacro ,name ,args (list 'lambda-yield () ,@body)))
+
 
