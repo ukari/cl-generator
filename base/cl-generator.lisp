9031283ce39d42ce831ab6c828549cdcf370816b
ukari <muromi.ukari@gmail.com>
2018-05-10 06:14:09 +0900

    add support for multiple values

--- base/cl-generator.lisp
+++ base/cl-generator.lisp
@@ -2,20 +2,30 @@
 
 (header)
 
-(defmacro yield (&optional arg)
-  (let ((x (gensym)))
-    `(call/cc (lambda (,x) (make-iterable-object :next ,x :value ,arg)))))
+(defmacro yield (&optional expr)
+  (let ((k (gensym))
+        (x (gensym))
+        (list (gensym "list")))
+    `(let ((,list (multiple-value-list ,expr)))
+       (print ,list)
+       (call/cc (lambda (,k) (values-list (or (mapcar (lambda (,x) (make-iterable-object :next ,k :value ,x))
+                                                                    ,list)
+                                                            (list (make-iterable-object :next ,k :value nil)))))))))
 
 (defmacro lambda* (args &body body)
-  `(lambda ,args
+  (let ((x (gensym)))
+    `(lambda ,args
        (with-call/cc
-	 (lambda ()
-	   (make-iterable-object :next nil :value (progn ,@body))))))
+	 (lambda () (values-list (or (mapcar (lambda (,x) (make-iterable-object :next nil :value ,x))
+                                             (multiple-value-list (progn ,@body)))
+                                     (list (make-iterable-object :next nil :value nil)))))))))
 
 (defmacro lambda-yield (&body body)
-  `(with-call/cc
-       (lambda ()
-	 (make-iterable-object :next nil :value (progn ,@body)))))
+  (let ((x (gensym)))
+    `(with-call/cc
+       (lambda () (values-list (or (mapcar (lambda (,x) (make-iterable-object :next nil :value ,x))
+                                           (multiple-value-list (progn ,@body)))
+                                   (list (make-iterable-object :next nil :value nil))))))))
 
 (defmacro defun* (name args &body body)
   `(defun ,name ,args (lambda-yield () ,@body)))
@@ -24,11 +34,21 @@
   `(defmacro ,name ,args (list 'lambda-yield () ,@body)))
 
 (defmacro yield* (expr)
-  (let ((x (gensym))
+  (let ((x (gensym "x"))
         (cont (gensym "cont")))
-    `(let ((,cont ,expr ))
+    `(let ((,cont ,expr))
        (if (eq (type-of ,cont) 'funcallable/cc)
-           (iterable-object-value (funcall ,cont))
+           (funcall ,cont)
            (if (listp ,cont)
                (loop for ,x in ,cont do (yield ,x))
                (error "invalid yield* argument"))))))
+
+
+
+;(defun* a () (yield 1) (yield 2) (values 3 4 5))
+
+;(defun* b () (yield (yield* (a))))
+;; 1 2 {3 4 5}
+
+;(defun* c () (yield* (a)))
+;; 1 2 
