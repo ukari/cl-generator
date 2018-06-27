5926bd4dcdb3013c6f074c597cc40388f84f3f4c
ukari <muromi.ukari@gmail.com>
2018-06-28 03:53:57 +0900

    add test unit for cl-generator-util package

--- t/util.lisp
+++ t/util.lisp
@@ -1,1 +1,12 @@
 (in-package :cl-generator-test)
+
+(defun* test-numbers ()
+  (let ((i 0))
+    (loop while (< i 10)
+       do (yield i)
+         (incf i))))
+
+(define-test for
+  (let ((collect nil))
+    (for (x (test-numbers)) (setf collect (append collect (list x))))
+    (assert-equal (list 0 1 2 3 4 5 6 7 8 9) collect)))
